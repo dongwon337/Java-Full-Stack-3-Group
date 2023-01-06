9d23dda11474bf29ee05c01d135e6957c338e2a2
dongwon337 <121939556+dongwon337@users.noreply.github.com>
2023-01-06 17:19:22 +0900

    Delete myjava directory

--- myjava/src/oopEx/Moveable.java
+++
@@ -1,7 +0,0 @@
-package oopEx;
-
-public interface Moveable {
-
-	void move(int i, int j);
-
-}
--- myjava/src/oopEx/packageEx/MarineSub.java
+++
@@ -1,8 +0,0 @@
-package oopEx.packageEx;
-
-public class MarineSub {
-
-	public static void main(String[] args) {
-		First f = new First(null, null);
-	}
-}
